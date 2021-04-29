1e4180dba184a2cf8429b412c7dcbeb1644fa38b
GlassK <glassk0724@gmail.com>
2021-04-29 18:19:03 +0900

    [124] String Methods Practice

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -415,9 +415,6 @@ console.log([...question]);
 console.log([...question.keys()]);
 console.log([...question.values()]);
 
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
-
 // Working With Strings - Part 1
 const airline = 'TAP Air Portugal';
 const plane = 'A320';
@@ -567,3 +564,21 @@ const planesInLine = function (n) {
 planesInLine(5);
 planesInLine(3);
 planesInLine(12);
+
+// String Methods Practice
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+const getCode = str => str.slice(0, 3).toUpperCase();
+
+for (const flight of flights.split('+')) {
+  const [type, from, to, time] = flight.split(';');
+  const output = `${type.startsWith('_Delayed') ? '🔴' : ''}${type.replaceAll(
+    '_',
+    ' '
+  )} from ${getCode(from)} to ${getCode(to)} ${time.replace(
+    ':',
+    'h'
+  )}`.padStart(50);
+  console.log(output);
+}
